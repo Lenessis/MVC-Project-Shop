4399b0f13403acfaf661d8019801765743f646ea
Lenessis <mkorecka@gmail.com>
2021-11-14 01:48:02 +0100

    init project. Add user model.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="strona_internetowa_mvc.MvcApplication" Language="C#" %>
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace strona_internetowa_mvc.Models
+{
+    public class User
+    {
+        /* -- Variables - basic information about common user -- */
+
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; } // Możliwe, ze będzie wymagał nowej klasy
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+
+
+    }
+}
